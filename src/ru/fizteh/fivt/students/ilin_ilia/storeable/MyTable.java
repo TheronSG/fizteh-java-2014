d784f9a553825c138bd84c4f890e2ccb391751aa
TheronSG <Ilinie@yandex.ru>
2014-12-10 23:08:52 +0300

    Update MyTable.java

--- src/ru/fizteh/fivt/students/ilin_ilia/storeable/MyTable.java
+++ src/ru/fizteh/fivt/students/ilin_ilia/storeable/MyTable.java
@@ -16,29 +16,32 @@ import java.util.List;
 
 public class MyTable implements Table {
     private Path name;
-    private int changesBeforeCommit;
     private int changesAfterCommit;
+    private List<String> commitKeys;
     /**
-     * inputFiles contains files which have read from db.
+     * inputFiles contains files which have read from db and won't change after commit.
      * changingFiles contains keys and values which will add during the programme.
-     * commitFiles contains keys and values after commit command.
      *
      * At the beginning of the programme all of them contain the same keys and values.
-     *
-     * isCommits shows us commit has been or no.
      *
      * isExists shows us file has been created before the programme or not.
      */
     private HashMap<Pair<Integer, Integer>, FileMap> inputFiles;
     private HashMap<Pair<Integer, Integer>, FileMap> changingFiles;
-    private HashMap<Pair<Integer, Integer>, FileMap> commitFiles;
     public static final String CODING_TYPE = "UTF-8";
     public static final int MAX_DIRECTORIES_AMOUNT  = 16;
     public static final int MAX_FILES_AMOUNT  = 16;
-    private boolean isCommits;
     private Class[] columnTypes;
     private boolean isExists;
     private TableProvider containsTableProvider;
+    /**
+     * If there are some uncommitted changes and we stop the programme
+     * this flag shows if we have printed the message about uncommitted
+     * changes.
+     * It's false if we printed it or there is no uncommitted changes.
+     * isInvitated true in current table and only in it.
+     */
+    private boolean isInvitated;
 
     MyTable(final String name, List<Class<?>> columnTypes, TableProvider tableProvider)
             throws IOException, ClassNotFoundException, ParseException {
@@ -49,11 +52,10 @@ public class MyTable implements Table {
                 this.columnTypes[i] = columnTypes.get(i);
             }
         }
-        isCommits = false;
+        commitKeys = new LinkedList<>();
         this.name = Paths.get(name);
         inputFiles = new HashMap<>();
         changingFiles = new HashMap<>();
-        commitFiles = new HashMap<>();
         File curTable = new File(name);
         if (curTable.exists()) {
             isExists = true;
@@ -68,8 +70,6 @@ public class MyTable implements Table {
                             inputFiles.put(pair, new FileMap(this.name.resolve(dir).resolve(fileName).toString(),
                                     this.name.resolve(dir).toString(), this, containsTableProvider));
                             changingFiles.put(pair, new FileMap(this.name.resolve(dir).resolve(fileName).toString(),
-                                    this.name.resolve(dir).toString(), this, containsTableProvider));
-                            commitFiles.put(pair, new FileMap(this.name.resolve(dir).resolve(fileName).toString(),
                                     this.name.resolve(dir).toString(), this, containsTableProvider));
                         }
                     }
@@ -114,8 +114,8 @@ public class MyTable implements Table {
         if (key == null) {
             throw new IllegalArgumentException("Can't get value. Empty key is impossible.");
         }
-        if (commitFiles.containsKey(convertIntoHashRule(key))) {
-            return commitFiles.get(convertIntoHashRule(key)).get(key);
+        if (changingFiles.containsKey(convertIntoHashRule(key))) {
+            return changingFiles.get(convertIntoHashRule(key)).get(key);
         } else {
             return null;
         }
@@ -133,14 +133,20 @@ public class MyTable implements Table {
         if (changingFiles.containsKey(pair)) {
             if (changingFiles.get(pair).containsKey(key)) {
                 if (!changingFiles.get(pair).get(key).equals(value)) {
-                    changesBeforeCommit++;
+                    if (!commitKeys.contains(key)) {
+                        commitKeys.add(key);
+                    }
                 }
             } else {
-                changesBeforeCommit++;
+                if (!commitKeys.contains(key)) {
+                    commitKeys.add(key);
+                }
             }
             return changingFiles.get(pair).put(key, value);
         } else {
-            changesBeforeCommit++;
+            if (!commitKeys.contains(key)) {
+                commitKeys.add(key);
+            }
             try {
                 changingFiles.put(pair,
                         new FileMap(name.resolve(pair.getKey().toString()).resolve(pair.getValue().toString()).toString(),
@@ -157,9 +163,12 @@ public class MyTable implements Table {
         if (key == null) {
             throw new IllegalArgumentException("Can't remove value for empty key.");
         }
+        if (!commitKeys.contains(key)) {
+            commitKeys.remove(key);
+        }
         Pair<Integer, Integer> pair = convertIntoHashRule(key);
-        if (commitFiles.containsKey(pair)) {
-            return commitFiles.get(convertIntoHashRule(key)).remove(key);
+        if (changingFiles.containsKey(pair)) {
+            return changingFiles.get(convertIntoHashRule(key)).remove(key);
         } else {
             return null;
         }
@@ -168,7 +177,7 @@ public class MyTable implements Table {
     @Override
     public int size() {
         int summ = 0;
-        for (FileMap fileMap : commitFiles.values()) {
+        for (FileMap fileMap : changingFiles.values()) {
             summ += fileMap.size();
         }
         return summ;
@@ -176,15 +185,12 @@ public class MyTable implements Table {
 
     @Override
     public int commit() {
-        if (!isCommits) {
-            isCommits = true;
-            commitFiles = changingFiles;
-        } else {
-            inputFiles = commitFiles;
-            commitFiles = changingFiles;
+        inputFiles.clear();
+        for (Pair<Integer, Integer> key : changingFiles.keySet()) {
+            inputFiles.put(key,changingFiles.get(key));
         }
-        changesAfterCommit = changesBeforeCommit;
-        changesBeforeCommit  = 0;
+        changesAfterCommit = commitKeys.size();
+        commitKeys.clear();
         return changesAfterCommit;
     }
 
@@ -192,16 +198,14 @@ public class MyTable implements Table {
     public int rollback() {
         int returnValue = changesAfterCommit;
         changesAfterCommit = 0;
-        changesBeforeCommit = 0;
+        commitKeys.clear();
         changingFiles = inputFiles;
-        commitFiles = inputFiles;
-        isCommits = false;
         return returnValue;
     }
 
     @Override
     public int getNumberOfUncommittedChanges() {
-        return changesBeforeCommit;
+        return commitKeys.size();
     }
 
     @Override
@@ -220,16 +224,12 @@ public class MyTable implements Table {
     @Override
     public List<String> list() {
         List<String> keys = new LinkedList<>();
-        for (FileMap fileMap : commitFiles.values()) {
+        for (FileMap fileMap : changingFiles.values()) {
             for (String listKey: fileMap.list()) {
                 keys.add(listKey);
             }
         }
         return keys;
-    }
-
-    public int getChangesBeforeCommit() {
-        return changesBeforeCommit;
     }
 
     public void saveDB() throws IOException {
@@ -251,12 +251,15 @@ public class MyTable implements Table {
                 throw new IOException("Can't create \"signature.tsv\" file for table \"" + name.toString() + "\"." );
             }
         }
-        for (FileMap fileMap : commitFiles.values()) {
+        for (FileMap fileMap : inputFiles.values()) {
             try {
                 fileMap.putFile();
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
+        }
+        if(commitKeys.size() != 0 && isInvitated) {
+            System.err.println(commitKeys.size() + " unsaved changes");
         }
     }
     public void readDB() throws IOException, ClassNotFoundException {
@@ -283,5 +286,10 @@ public class MyTable implements Table {
                 throw new ClassNotFoundException("Convert error! Class \"" + types.get(i) + "\" doesn't exist.");
             }
         }
+        fileSignature.close();
+    }
+
+    public void setIsInvitatedTrue() {
+        isInvitated = true;
     }
 }
